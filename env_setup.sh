d8341524e64a29c2d588cb4c68259ffe53279f5f
cetaSYN <46914689+cetaSYN@users.noreply.github.com>
2019-01-25 07:56:03 -0800

    Move project to higher folder and add env_setup.sh

---
+++ env_setup.sh
@@ -0,0 +1,23 @@
+# Reference: ad2dispatch/settings.py
+
+export AD2_DOMAIN='local_test.lan'
+export AD2_ORG_NAME='Local Test'
+export AD2_ORG_ACRONYM='LCLTEST'
+export AD2_ORG_PHONE='+01234567890'
+export AD2_ORG_PHONE_DISPLAY='(123) 456-7890'
+
+export AD2_SECRET_KEY='00000000'
+export AD2_DEBUG='True'
+export AD2_ALLOWED_HOSTS='*'
+
+# Modify for your own test env
+export AD2_DB_HOST='127.0.0.1'
+export AD2_DB_NAME='ad2dispatch'
+export AD2_DB_USER='ad2dispatch'
+export AD2_DB_PASSWORD='password'
+export AD2_DB_PORT='3306'
+
+# Ignore unless needed
+# export AWS_SES_ACCESS_KEY_ID=
+# export AWS_SES_SECRET_ACCESS_KEY=
+# export AWS_SES_REGION_NAME=
