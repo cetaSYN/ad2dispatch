8239965afc1ec47f11bb467832a86bb3c855a1b6
cetaSYN <46914689+cetaSYN@users.noreply.github.com>
2019-02-09 15:26:23 -0800

    Resolve #8: Generifys views and enables viewing of event-specific details

--- ad2dispatch/events/urls.py
+++ ad2dispatch/events/urls.py
@@ -5,12 +5,11 @@ from . import views
 app_name = 'events'
 urlpatterns = [
     url(r'^$', views.upcoming_list, name='upcoming_list'),
-    url(r'^driver/$', views.driver, name='driver'),  # TODO Don't hardcode
-    url(r'^dispatcher/$', views.dispatcher, name='dispatcher'),
     url(r'^manage/$', views.manage, name='manage'),
     url(r'^manage/' +
         r'(?P<start_date>[0-9]{2}[A-Za-z]{3}[0-9]{2})/' +
         r'(?P<end_date>[0-9]{2}[A-Za-z]{3}[0-9]{2})/$',
         views.manage, name='manage'),
-    url(r'^event/(?P<event_id>[0-9]+)/$', views.event, name='event')
+    url(r'^event/(?P<event_id>[0-9]+)/manage/$', views.event_manage, name='event_manage'),
+    url(r'^event/(?P<event_id>[0-9]+)/(?P<type_name>[A-Za-z_0-9\-]+)/$', views.event, name='event'),
 ]
